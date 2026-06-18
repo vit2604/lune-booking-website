b2bb5f8d6f33ab5e7bfd0e37c80f39017cbb5473
vit2604 <dangtrungvuong2020@gmail.com>
2026-06-19 00:56:06 +0700

    Handle empty request bodies in validation

--- server/src/middlewares/validateMiddleware.js
+++ server/src/middlewares/validateMiddleware.js
@@ -3,9 +3,9 @@ import { createHttpError } from '../utils/responseUtils.js';
 export function validate(schema) {
   return (req, _res, next) => {
     const result = schema.safeParse({
-      body: req.body,
-      query: req.query,
-      params: req.params,
+      body: req.body || {},
+      query: req.query || {},
+      params: req.params || {},
     });
 
     if (!result.success) {
